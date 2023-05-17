4f10965789a4e4a785b1599cbe774d0f0aa10f6c
Rainbow-Falls <71909085+Rainbow-Falls@users.noreply.github.com>
2023-05-17 15:23:31 -0700

    Finish the Main Menu Buttons and Background

--- IDP-Group1-2023/Assets/Media/Main Menu/StartButton.cs
+++ IDP-Group1-2023/Assets/Media/Main Menu/StartButton.cs
@@ -3,22 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneChange : MonoBehaviour
+public class StartButton : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void ChangeScene()
+    public void GameplaySceneChange()
     {
         SceneManager.LoadScene("Gameplay");
     }
+    public void LeaderboardSceneChange()
+    {
+        SceneManager.LoadScene("Leaderboard");
+    }
+    public void ControlsSceneChange()
+    {
+        SceneManager.LoadScene("Controls");
+    }
 }
+
