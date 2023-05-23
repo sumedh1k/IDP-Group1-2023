4f2728ee155625e6cd510ef45a01873e14818379
Rainbow-Falls <71909085+Rainbow-Falls@users.noreply.github.com>
2023-05-23 01:34:04 -0700

    Make Dialogue and Inventory
    There is a merge conflict so there will be a part 2 to this.

--- IDP-Group1-2023/Assets/Scripts/Living Room/CipherBook.cs
+++ IDP-Group1-2023/Assets/Scripts/Living Room/CipherBook.cs
@@ -6,16 +6,18 @@ public class CipherBook : MonoBehaviour
 {
     public GameObject CipherBookObject;
     public GameObject CipherBookSide;
+    public GameObject CipBookInvSpot;
 
     public void OpenCipherBook()
     {
 
-        Debug.Log("Clicked");
+        Debug.Log("Cipher Book Clicked");
 
         if (CipherBookSide != null)
         {
             CipherBookObject.SetActive(false);
             CipherBookSide.SetActive(false);
+            CipBookInvSpot.SetActive(true);
         }
 
     }
