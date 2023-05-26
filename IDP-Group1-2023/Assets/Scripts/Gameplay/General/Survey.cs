a08c632da1ec8f09d4422bd9f1ef4b4c02b0184a
Sumedh Karajagi <122140290+sumedh1k@users.noreply.github.com>
2023-05-26 09:39:49 -0700

    Working feedback survey using google sheets api

--- IDP-Group1-2023/Assets/Scripts/Gameplay/General/Survey.cs
+++ IDP-Group1-2023/Assets/Scripts/Gameplay/General/Survey.cs
@@ -19,20 +19,16 @@ public class Survey : MonoBehaviour
 
     public void Send()
     {
-        StartCoroutine(Post(feedback1.text));
-        StartCoroutine(Post(feedback2.text));
-        StartCoroutine(Post(feedback3.text));
-        StartCoroutine(Post(feedback4.text));
-        StartCoroutine(Post(feedback5.text));
+        StartCoroutine(Post(feedback1.text, feedback2.text, int.Parse(feedback3.text), int.Parse(feedback4.text), feedback5));
     }
 
     IEnumerator Post()
     {
-        WWWForm form = new WWWForm();
-        form.AddField("entry.1438025189", ); //Q1
+        WWWForm form = new WWWForm(string s1, string s2, int num1, int num2, string s3 );
+        form.AddField("entry.1438025189", s1); //Q1
         form.AddField("entry.303991439", s2); //Q2
-        form.AddField("entry.169532271", i3); //Q3
-        form.AddField("entry.330256318", i4); //Q4
+        form.AddField("entry.169532271", num1.ToString()); //Q3
+        form.AddField("entry.330256318", num2.ToString()); //Q4
         form.AddField("entry.1498743572", s3); //Q5
 
         UnityWebRequest www = UnityWebRequest.Post(URL, form);
@@ -43,3 +39,51 @@ public class Survey : MonoBehaviour
 
 }
 */
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using System.Linq;
+
+public class Survey : MonoBehaviour
+{
+    [SerializeField] InputField feedback1;
+    [SerializeField] InputField feedback2;
+    [SerializeField] ToggleGroup toggleGroup3;
+    [SerializeField] ToggleGroup toggleGroup4;
+    [SerializeField] InputField feedback5;
+
+    string URL = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSforM0ZFPLKcNTqCxeLNVxT-vG1SJRnsF6-oQBnAUxuxb-puw/formResponse";
+
+    public void Send()
+    {
+        int value3 = GetToggleGroupValue(toggleGroup3);
+        int value4 = GetToggleGroupValue(toggleGroup4);
+        StartCoroutine(Post(feedback1.text, feedback2.text, value3, value4, feedback5.text));
+    }
+
+    int GetToggleGroupValue(ToggleGroup toggleGroup)
+    {
+        Toggle activeToggle = toggleGroup.ActiveToggles().FirstOrDefault();
+        if (activeToggle != null)
+        {
+            int index = activeToggle.transform.GetSiblingIndex();
+            return index + 1;
+        }
+        return 0;
+    }
+
+    IEnumerator Post(string s1, string s2, int num1, int num2, string s3)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("entry.1438025189", s1); //Q1
+        form.AddField("entry.303991439", s2); //Q2
+        form.AddField("entry.169532271", num1.ToString()); //Q3
+        form.AddField("entry.330256318", num2.ToString()); //Q4
+        form.AddField("entry.1498743572", s3); //Q5
+
+        UnityWebRequest www = UnityWebRequest.Post(URL, form);
+        yield return www.SendWebRequest();
+    }
+}
